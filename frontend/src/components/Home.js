b803a819cd7183f4b7ccd4f63e4263e744b8a2c6
JALLA VINAY KUMAR <vinayjallamruh@gmail.com>
2025-11-10 16:45:58 +0530

    Update image sources in Home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -17,17 +17,17 @@ const Home = () => (
 
       <div className="feature-images">
         <Link to="/weather" className="image-box">
-          <img src="https://cdn-icons-png.flaticon.com/512/869/869869.png" alt="Weather"/>
+          <img src="https://tse2.mm.bing.net/th/id/OIP.NEgCdeNDoFA5XHRiZKjS8wHaHa?pid=Api&P=0&h=180" alt="Weather"/>
           <button className="btn btn-info">Weather</button>
         </Link>
 
         <Link to="/currency-converter" className="image-box">
-          <img src="https://cdn-icons-png.flaticon.com/512/1444/1444797.png" alt="Currency"/>
+          <img src="https://tse1.mm.bing.net/th/id/OIP.UKEuMwSysA-hwu-2XpAXNQHaHa?pid=Api&P=0&h=180" alt="Currency"/>
           <button className="btn btn-info">Currency</button>
         </Link>
 
         <Link to="/quotes" className="image-box">
-          <img src="https://media.istockphoto.com/id/1473443216/vector/stop-thinking-start-doing-vector-illustration.jpg?s=612x612&w=0&k=20&c=pKvB5xT_ORTfDj2_6f5lgMHa0EZ3SxSomkBeBpzK1ck=" alt="Quotes"/>
+          <img src="https://tse3.mm.bing.net/th/id/OIP.IeS6ywYMekXNHZolCpfdcgHaH0?pid=Api&P=0&h=180" alt="Quotes"/>
           <button className="btn btn-info">Quotes</button>
         </Link>
       </div>
